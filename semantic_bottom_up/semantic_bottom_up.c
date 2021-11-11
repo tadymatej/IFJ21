8fc056ebfafbffa410a537f4a8c2169c52f79d9b
juraj-juraj <xnovos13@vutbr.cz>
2021-11-11 23:23:48 +0100

    first commit on semantic_bottom _up. Only head comments

---
+++ semantic_bottom_up/semantic_bottom_up.c
@@ -0,0 +1,9 @@
+/********************************************************************************
+ *  Projekt - Prekladač
+ *  Súbor: semantic_bottom_up.c
+ *  Popis: Implementácia sémantickej kontroly zdola hore
+ *  Zodpovedný študent: Juraj Novosád email: xnovos13 @stud.fit.vutbr.cz
+ ********************************************************************************
+*/
+
+//koniec súboru semantic_bottom_up.c
---
+++ semantic_bottom_up/semantic_bottom_up.h
@@ -0,0 +1,10 @@
+/********************************************************************************
+ *  Projekt - Prekladač
+ *  Súbor: semantic_bottom_up.h
+ *  Popis: Implementácia sémantickej kontroly zdola hore
+ *  Zodpovedný študent: Juraj Novosád email: xnovos13 @stud.fit.vutbr.cz
+ ********************************************************************************
+*/
+
+
+//koniec súboru semantic_bottom_up.h
