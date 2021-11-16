c022f03e1a147e1903a4aeb6954ba4ebc0cb1c23
Eva Micankova <81229113+xmican10@users.noreply.github.com>
2021-11-16 23:22:16 +0100

    Add files via upload

---
+++ parser/parser.h
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "../scanner/scanner.h"
+
+bool Parse();
+/**
+ * Vytvoří nový token
+ * @param attribute Atribut tokenu
+*/
+void PrintData(char *attribute);
