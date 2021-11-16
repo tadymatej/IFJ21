6c99dd7754c3e88ed3963d163462ce548bbc1e29
Eva Micankova <81229113+xmican10@users.noreply.github.com>
2021-11-16 23:23:50 +0100

    Add files via upload

---
+++ main.c
@@ -0,0 +1,11 @@
+#include "./scanner/scanner.h"
+#include "./parser/parser.h"
+//#include "scanner.c"
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+int main(){
+    bool syntaxOK = Parse();
+    printf("syntax OK: %d\n", syntaxOK);
+}
