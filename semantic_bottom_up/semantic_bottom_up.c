b68ebb27e378db8164888168f4fc2bda3fa0d330
juraj-juraj <xnovos13@vutbr.cz>
2021-11-22 19:01:10 +0100

    added comments and structures

--- semantic_bottom_up/semantic_bottom_up.c
+++ semantic_bottom_up/semantic_bottom_up.c
@@ -11,14 +11,18 @@
 void do_action(exp_tree_stack_t *stack, Token *token, int nest_level){
   switch (token->token_type) {
     case TOKEN_ID: case TOKEN_NUMBER: case TOKEN_STRING: case TOKEN_NUMBER_INT:
+      //skontroluj ci je definovana premenna
       add_id_node(stack, NULL, ATTRIBUTE_INTEGER, token->token_type); //pozna atributy INTEGER, STRING, NONE, NUMBER
       break;
     case TOKEN_LEN:
+      //ak je to na vrchole typu string
       unary_operator(stack);
       break;
     case TOKEN_END_BRACKET:
       break;
     default:
+      //skontroluj ci su dva njevrchnejise operatory kompatibilneho typu
+      // potom skontroluj ci je ich vysledny typ kompatiilny s operatorom
       operator_merge(stack, token->token_type, ATTRIBUTE_INTEGER);
       break;
   }
