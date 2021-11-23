706b6f6fbb12419803bf39788901b4e3abdd51c8
stSavist <xsauch00@stud.fit.vutbr.cz>
2021-11-23 15:00:25 +0100

    Add fun_is_equal

--- libraries/fun_data.h
+++ libraries/fun_data.h
@@ -15,5 +15,6 @@ Fun_data_t* init_fun_data(char *name);
 int fun_add_param(Fun_data_t *data, TS_data_t *param);
 int fun_add_ret_val(Fun_data_t *data, TS_data_t *param);
 int dispose_fun_data(Fun_data_t **data);
+bool fun_is_equal(Fun_data_t *first, Fun_data_t *second);
 
 #endif
