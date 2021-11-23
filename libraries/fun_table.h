ffb081f1dcd585ca5fb3d6f28a8bddd4bbe0b55c
stSavist <xsauch00@stud.fit.vutbr.cz>
2021-11-23 23:15:14 +0100

    Partially integrated support for builtin functions

--- libraries/fun_table.h
+++ libraries/fun_table.h
@@ -7,12 +7,13 @@
 typedef struct Fun_table {
     TreeNode *defFunTree;
     TreeNode *decFunTree;
+    TreeNode *builtinFunTree;
 } Fun_table_t;
 
 Fun_table_t *init_fun_table();
 void add_function_dec(Fun_table_t *table, Fun_data_t *fun);
 void add_function_def(Fun_table_t *table, Fun_data_t *fun);
-Fun_data_t *find_function(Fun_table_t *table, char *fun_name, bool *isOnlyDeclared);
+Fun_data_t *find_function(Fun_table_t *table, char *fun_name, bool *isOnlyDeclared, bool *isBuiltIn);
 bool is_dec_and_def_equal(Fun_table_t *table, Fun_data_t *fun);
 void dispose_fun_table(Fun_table_t **table);
 
