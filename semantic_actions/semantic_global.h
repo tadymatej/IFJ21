11ff67e528fe789249f86178417d85228cf01d76
stSavist <xsauch00@stud.fit.vutbr.cz>
2021-11-23 15:15:48 +0100

    Change SemanticGlobals definition. Add init function definition

--- semantic_actions/semantic_global.h
+++ semantic_actions/semantic_global.h
@@ -1,11 +1,23 @@
 #ifndef __SEM_GLOBALS_H_
 #define __SEM_GLOBALS_H_ 1
 
+#include "symtable.h"
+#include "TS_handler.h"
+#include "fun_table.h"
 #include "fun_data.h"
+#include "queue.h"
 
 typedef struct SemanticGlobals{
     int nested_count;
     Fun_data_t *cur_function;
+    TS_data_t *var;
+    int tmp;
+    Queue_t *q_assignments;
+    Sym_table_t *ts;
+    Fun_table_t *ft;
 }SemanticGlobals;
+
+int init_sem_globals();
+
 
 #endif
