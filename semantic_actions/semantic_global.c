a23b154ef30737eec9938d33fb53f027acc580aa
juraj-juraj <xnovos13@vutbr.cz>
2021-11-24 11:33:03 +0100

    comments on semantic globals. to ammend warnings

--- semantic_actions/semantic_global.c
+++ semantic_actions/semantic_global.c
@@ -13,18 +13,18 @@ int init_sem_globals() {
     globals.blockStack = stack_init();
     globals.q_command = init_queue();
     if (globals.q_assignments == NULL || globals.ts == NULL || globals.ft == NULL || globals.blockStack == NULL || globals.q_command == NULL) {
-        
+
         return 99;
     }
     return 0;
 }
 
 void dispose_sem_globals() {
-    dispose_fun_data(globals.cur_function);
-    dispose_queue(globals.q_assignments);
-    dispose_queue(globals.q_command);
+    dispose_fun_data(globals.cur_function); //tu by malo byt &globals.cur_function
+    dispose_queue(globals.q_assignments); //aj tu
+    dispose_queue(globals.q_command);      // aj tu
     free(globals.var);
-    dispose_fun_table(&globals.ft);
+  //  dispose_fun_table(&globals.ft);  //TODO vyhadzuje to undefined reference
     dispose_table(&globals.ts);
     stack_destroy(&globals.blockStack);
 }
