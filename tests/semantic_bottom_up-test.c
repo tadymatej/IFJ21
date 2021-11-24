94e9fe73e7d437ec0a313f0e4077ad9d1bf730c5
juraj-juraj <xnovos13@vutbr.cz>
2021-11-24 17:51:22 +0100

    further updates to test

--- tests/semantic_bottom_up-test.c
+++ tests/semantic_bottom_up-test.c
@@ -16,9 +16,9 @@ int main() {
 	strArr = StringsArrayCreate('\0');
 
   init_sem_globals();
-  globals.ts = TS_init("LF");
-	printf("po inite\n");
+  globals.ts = TS_init("TF");
   TS_data_t *temp_data;
+
   temp_data = make_var_data(INTEGER, "var1", "7");
   add_variable(globals.ts, temp_data);
   temp_data = make_var_data(INTEGER, "var2", "5");
@@ -34,11 +34,22 @@ int main() {
   add_variable(globals.ts, temp_data);
   temp_data = make_var_data(STRING, "var3", "456");
   add_variable(globals.ts, temp_data);
+
   printf("po vkladani\n");
 
   Sym_table_t *TS_temp;
-  temp_data = find_variable(globals.ts, "var1", &TS_temp);
+	temp_data = find_variable(globals.ts, "str1", &TS_temp);
+	//q_push(globals.q_assignments, temp_data);
   printf("temp meno: %s || hodnota: %s || v strome: %d\n", temp_data->name, temp_data->value, TS_temp->nested_identifier);
+
+  temp_data = find_variable(globals.ts, "num1", &TS_temp);
+	//q_push(globals.q_assignments, temp_data);
+  printf("temp meno: %s || hodnota: %s || v strome: %d\n", temp_data->name, temp_data->value, TS_temp->nested_identifier);
+
+	temp_data = find_variable(globals.ts, "str1", &TS_temp);
+	//q_push(globals.q_assignments, temp_data);
+  printf("temp meno: %s || hodnota: %s || v strome: %d\n", temp_data->name, temp_data->value, TS_temp->nested_identifier);
+
 
 	error_flag = precedence_analyzer(&sc);
 	printf("ukoncene spracovanie textu, navratova hodnota: %d\n", error_flag);
