561555a52baebd7e205bf74c973f955d49dad3c0
Eva Micankova <81229113+xmican10@users.noreply.github.com>
2021-11-24 23:24:13 +0100

    main update

--- main.c
+++ main.c
@@ -1,11 +1,14 @@
 #include "./scanner/scanner.h"
 #include "./parser/parser.h"
+#include "./semantic_actions/semantic_global.h"
 //#include "scanner.c"
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
 
 int main(){
+    init_sem_globals();
+    globals.ts = TS_init("TF");
     bool syntaxOK = Parse();
     printf("syntax OK: %d\n", syntaxOK);
 }
