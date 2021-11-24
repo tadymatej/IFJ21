19e66f1bf4aedc4001981e9e0f66ef90f3b87b6b
juraj-juraj <xnovos13@vutbr.cz>
2021-11-24 11:34:34 +0100

    fixed semantic bottom up test and added support in makefile

--- Makefile
+++ Makefile
@@ -2,6 +2,7 @@ SIMPLE_STACK=simple_stack
 ANALYZER=precedence_analyzer
 SCANNER_PRECEDENCE=scanner_precedence
 SEMANTIC_BOTTOM_UP=semantic_bottom_up
+SEMANTIC_GLOBAL=semantic_global
 #
 PROGS=$(SIMPLE_STACK)-test $(ANALYZER)-test
 LIB_PATH=libraries/
@@ -10,6 +11,9 @@ TEST_PATH=tests/
 SCANNER_PATH=scanner/
 SEMANTIC_BOTTOM_PATH=semantic_bottom_up/
 SEMANTIC_ACTIONS=semantic_actions/
+
+SEMANTIC_BOTTOM_UP_DEPS=$(SEMANTIC_ACTIONS)$(SEMANTIC_GLOBAL).c $(SCANNER_PATH)scanner.c $(ANALYZER_PATH)$(ANALYZER).c $(SEMANTIC_BOTTOM_PATH)*.c $(LIB_PATH)*.c
+
 CC=gcc
 CFLAGS=-std=c99 -Wall -Wextra -pedantic -lm -I$(LIB_PATH) -I$(ANALYZER_PATH) -I$(SCANNER_PATH) -I$(SEMANTIC_BOTTOM_PATH) -I$(SEMANTIC_ACTIONS) -fcommon -g
 
@@ -24,7 +28,7 @@ run_analyzer: $(SCANNER_PRECEDENCE)-test
 	echo "((#a+8)/c)-(2*(a+9)) if" | build/$(SCANNER_PRECEDENCE)-test
 
 run_bottom_up: $(SEMANTIC_BOTTOM_UP)-test
-	build/$(SEMANTIC_BOTTOM_UP)-test
+	echo "var1+var2*5 if" | build/$(SEMANTIC_BOTTOM_UP)-test
 
 $(SIMPLE_STACK)-test: $(TEST_PATH)$(SIMPLE_STACK)-test.c $(LIB_PATH)$(SIMPLE_STACK).c
 	mkdir -p build/
@@ -34,7 +38,7 @@ $(SCANNER_PRECEDENCE)-test: $(TEST_PATH)$(SCANNER_PRECEDENCE)-test.c $(ANALYZER_
 	mkdir -p build/
 	$(CC) $(CFLAGS) -o build/$@ $^
 
-$(SEMANTIC_BOTTOM_UP)-test: $(TEST_PATH)$(SEMANTIC_BOTTOM_UP)-test.c $(ANALYZER_PATH)$(ANALYZER).c $(SCANNER_PATH)scanner.c $(SEMANTIC_BOTTOM_PATH)*.c $(LIB_PATH)*.c
+$(SEMANTIC_BOTTOM_UP)-test: $(TEST_PATH)$(SEMANTIC_BOTTOM_UP)-test.c $(SEMANTIC_BOTTOM_UP_DEPS)
 	mkdir -p build/
 	$(CC) $(CFLAGS) -o build/$@ $^
 
--- tests/semantic_bottom_up-test.c
+++ tests/semantic_bottom_up-test.c
@@ -3,40 +3,44 @@
 #include <string.h>
 #include <assert.h>
 #include "scanner.h"
+#include "TS_handler.h"
 #include "precedence_analyzer.h"
 #include "semantic_bottom_up.h"
 
 int error_flag;
 
-Sym_table_t *TS;
-
 int main() {
 	ScannerContext sc;
 	sc.lastReadedChar = -1;
-
-	ScannerContextInit(&sc);
+  ScannerContextInit(&sc);
 	strArr = StringsArrayCreate('\0');
 
-  SemanticGlobals.nested_count = 0;
-
-  TS = TS_init();
-  printf("po inite\n");
+  init_sem_globals();
+  globals.ts = TS_init();
+	printf("po inite\n");
   TS_data_t *temp_data;
   temp_data = make_var_data(INTEGER, "var1", "7");
-  add_variable(TS, temp_data);
+  add_variable(globals.ts, temp_data);
   temp_data = make_var_data(INTEGER, "var2", "5");
-  add_variable(TS, temp_data);
+  add_variable(globals.ts, temp_data);
   temp_data = make_var_data(STRING, "str1", "ahoj");
-  add_variable(TS, temp_data);
+  add_variable(globals.ts, temp_data);
   temp_data = make_var_data(NUMBER, "num1", "7.85");
-  add_variable(TS, temp_data);
+  add_variable(globals.ts, temp_data);
+  globals.nested_count++;
+  new_stack_frame(&(globals.ts));
+  temp_data = make_var_data(STRING, "str1", "ine");
+  add_variable(globals.ts, temp_data);
+  temp_data = make_var_data(STRING, "var3", "456");
+  add_variable(globals.ts, temp_data);
   printf("po vkladani\n");
 
   Sym_table_t *TS_temp;
-  temp_data = find_variable(TS, "var2", &TS_temp);
-  printf("temp meno: %s || hodnota: %s\n", temp_data->name, temp_data->value);
+  temp_data = find_variable(globals.ts, "var1", &TS_temp);
+  printf("temp meno: %s || hodnota: %s || v strome: %d\n", temp_data->name, temp_data->value, TS_temp->nested_identifier);
 
-	//error_flag = precedence_analyzer(&sc);
+	error_flag = precedence_analyzer(&sc);
 	printf("ukoncene spracovanie textu, navratova hodnota: %d\n", error_flag);
-	return (0);
+	dispose_sem_globals();
+	return 0;
 }
