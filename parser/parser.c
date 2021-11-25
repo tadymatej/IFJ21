352c7ee19bce6c9fbcfe03ceeea9a2b7154002f4
Eva Micankova <81229113+xmican10@users.noreply.github.com>
2021-11-25 17:12:32 +0100

    err handling

--- parser/parser.c
+++ parser/parser.c
@@ -9,28 +9,28 @@
 #define DEBUG_ERROR
 
 
-int isEnd = 0;
+bool isEnd = false;
+bool err = false;
 
-void ErrMessage(int errType){
-    if(errType == COMPILER_ERR){
-        fprintf(stderr, "%d\n", COMPILER_ERR);
-        fprintf(stderr, "Interni chyba prekladace: \n");
-    } else if(errType == SYNTAX_ERR){
-        fprintf(stderr, "%d\n", SYNTAX_ERR);
-        fprintf(stderr, "Syntakticka chyba: \n");
-    }
+void ErrMessagePossition(Token *ptr, ScannerContext *sc){
+    fprintf(stderr, "Chyba na radku: %d a sloupci: %d\n", ptr->startPosRow, ptr->startPosCol);
     return;
 }
 
-void ErrMessagePossition(Token *ptr, ScannerContext *sc){
-    //Obsluha chyby mimo lexikalni analyzu
-    int len = strlen(ptr->attribute);
-    sc->col -= len;
+void ErrMessage(int errType){
+    if(errType == LEX_ERR){
+        fprintf(stderr, "%d\n", LEX_ERR);
+        fprintf(stderr, "Lexikalni chyba: ");
 
-    //Vypis chybu
-    //sc->col += len;
+    } else if(errType == COMPILER_ERR){
+        fprintf(stderr, "%d\n", COMPILER_ERR);
+        fprintf(stderr, "Interni chyba prekladace: ");
+    } else if(errType == SYNTAX_ERR){
 
-    fprintf(stderr, "Chyba na radku: %d a sloupci: %d\n", sc->row, sc->col);
+        fprintf(stderr, "%d\n", SYNTAX_ERR);
+        fprintf(stderr, "Syntakticka chyba: ");
+    }
+
     return;
 }
 
@@ -47,26 +47,23 @@ Token Next(ScannerContext *sc){
 
         } else if (token.token_type == TOKEN_ERR){
             // lexikalni chyba
-            fprintf(stderr, "%d\n", LEX_ERR);
-
-            fprintf(stderr, "Chyba na radku: %d a sloupci: %d\n", sc->row, sc->col);
-            //sc->actualState = STATE_START;
+            ErrMessage(LEX_ERR);
+            fprintf(stderr, "Chyba na radku: %d a sloupci: %d\n", token.startPosRow, token.startPosCol);
+            
+            err = true;
 
             exit(EXIT_FAILURE); // TODO odstranit exit
         }
     }
 
     else if(token.token_type == TOKEN_NONE){
-        // TODO
         // doceten vstup do konce
 
-        //TokenStore(token, sc);
-        //printf("konec\n");
-        //isEnd++;
+        // TODO
+        isEnd = true;
     }
 
     return token;
-
 }
 
 // ------------------------------------------------------------------
@@ -367,6 +364,7 @@ bool NExp(Token *ptr, ScannerContext *sc){
 
     if(psa != 0){
         ErrMessage(psa);
+        ErrMessagePossition(ptr, sc);
         exit(EXIT_FAILURE);
     }
 
@@ -461,6 +459,7 @@ bool NExp_cond(Token *ptr, ScannerContext *sc){
 
     if(psa != 0){
         ErrMessage(psa);
+        ErrMessagePossition(ptr, sc);
         exit(EXIT_FAILURE);
     }
     
@@ -709,7 +708,7 @@ bool NFunction_body(Token *ptr, ScannerContext *sc){
     bool function_body = true;
     int break_from_while = 0;
 
-    while((ptr->token_type != TOKEN_NONE || sc->actualState == STATE_ERR) && function_body == true && isEnd == 0){
+    while((ptr->token_type != TOKEN_NONE || sc->actualState == STATE_ERR) && function_body == true){
         //printf("NFunction_body accepted: \t%s \t%s\n", lex2String(ptr->token_type), ptr->attribute);
 
         //function_body = true;
