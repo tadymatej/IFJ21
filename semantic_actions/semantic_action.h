f35742746d0497a36110d0e46f01693cfa90c91a
stSavist <xsauch00@stud.fit.vutbr.cz>
2021-11-25 22:32:47 +0100

    Temprorary comment for fix syntax errors. NOT TESTED. HAS ERRORS Fix functions. Add functions and macroses. Macroses : NOT_SUCCESS(expr) - return true if error occured ASSIGNMENT_TYPE_CHECK(left,right, error_code) - type control for assignments RET_IF_NOT_SUCCESS(expr) - return INTERNAL_ERROR if error occured Functions : jump_to_exec_point - generate command "jump" to next global function call end_program - generate label at the end of program, for correct behaviour of execuiton point concept before_global_fun_call - generate label before function call in global frame. For correct behaviour of execution point concept after_global_fun_call - generate command "jump" to next execution point

--- semantic_actions/semantic_action.h
+++ semantic_actions/semantic_action.h
@@ -2,14 +2,34 @@
 #define __SEM_ACTION_H_ 1
 
 #include "semantic_global.h"
+#include "semantic_bottom_up.h"
 #include "code_generator.h"
 #include "scanner.h"
 #include <stdlib.h>
 
-#define ITOA(bufName, num) \
-    char bufName[256];         \
-    snprintf(bufName, 256, "%d", num)
+#define DEFINITON_ERROR 3
+#define ASSIGNMENT_TYPE_ERROR 4
+#define FUN_CALL_ERROR 5
+#define EXPR_TYPE_ERRORS 6
+#define OTHER_SEM_ERRORS 7
+#define INTERNAL_ERROR 99
+#define SEM_CORRECT 0
 
+#define NOT_SUCCESS(expr) expr != SEM_CORRECT
+
+/* 7 - assignment*/
+#define ASSIGNMENT_TYPE_CHECK(left, right, error_code) \
+    if (ret_types_table[7][(left)][(right)] == NO_TYPE)     \
+        return error_code;
+
+#define RET_IF_NOT_SUCCESS(expr) \
+    if (NOT_SUCCESS(expr))       \
+    return INTERNAL_ERROR
+
+int jump_to_exec_point();
+int end_program();
+int before_global_fun_call();
+int after_global_fun_call();
 int function_definition(Token *token);
 int init_function_argument(Token *token);
 int var_type_assignment(Token *token);
