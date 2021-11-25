67faf277e27d8fa1b9063ca90129e1a400340574
Matej <xzalma00@stud.fit.vutbr.cz>
2021-11-25 07:54:13 +0100

    Přidání nutných úprav knihoven, oprava chyby v queue.c - pop nefungovalo

---
+++ libraries/queue.h
@@ -0,0 +1,39 @@
+/********************************************************************************
+ *  Projekt - Prekladač
+ *  Soubor: queue.h
+ *  Popis: knihovna pro práci s jednosmerne vazanym seznamem celych cisel
+ *  Soubor obsahuje realizace seznamu pro praci s (void*)
+ *  Zodpovedný student: Igar Sauchanka email: xsauch00@stud.fit.vutbr.cz
+ ********************************************************************************
+*/
+
+#ifndef __QUEUE_H_
+#define __QUEUE_H_ 1
+
+#define DEFAULT_QUEUE_SIZE 10
+
+#include "symtable.h"
+
+typedef struct Q_Elem {
+    void *data;
+    struct Q_Elem *next;
+}Q_elem_t;
+
+typedef struct Queue {
+    Q_elem_t *first;
+    Q_elem_t *last;
+}Queue_t;
+
+Queue_t* init_queue();
+int q_push(Queue_t *q, void *elem);
+void* q_top(Queue_t *q);
+void* q_pop(Queue_t *q);
+void dispose_queue(Queue_t **q);
+
+/**
+ * Vloží na konec fronty a provede swap dvou posledních elementů
+ */ 
+int q_push_front(Queue_t *q, void *elem);
+
+#endif
+// Konec souboru queue.h
