fc50084269310714a045429d88d8523a4f58c9dd
stSavist <xsauch00@stud.fit.vutbr.cz>
2021-12-08 13:27:56 +0100

    Add builtin functions initialization

--- libraries/fun_table.h
+++ libraries/fun_table.h
@@ -10,11 +10,17 @@ typedef struct Fun_table {
     TreeNode *builtinFunTree;
 } Fun_table_t;
 
+#define BUILTIN_COUNT 8
+
+#ifndef _TS_HANDLER_
+TS_data_t *make_var_data(DataTypes_t type, char *name, char *value);
+void dispose_ts_data(void *ptr);
+#endif
+
 Fun_table_t *init_fun_table();
 int add_function_dec(Fun_table_t *table, Fun_data_t *fun);
 int add_function_def(Fun_table_t *table, Fun_data_t *fun);
 Fun_data_t *find_function(Fun_table_t *table, char *fun_name, bool *isOnlyDeclared, bool *isBuiltIn);
 bool is_dec_and_def_equal(Fun_table_t *table, Fun_data_t *fun);
 void dispose_fun_table(Fun_table_t **table);
-
 #endif
