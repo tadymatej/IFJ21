b9d0e282c02e299a3b3cad5577bbcb9e72c41fa4
juraj-juraj <xnovos13@vutbr.cz>
2021-12-08 02:08:57 +0100

    if labels fixed

--- libraries/cg_expression_tree.c
+++ libraries/cg_expression_tree.c
@@ -134,8 +134,8 @@ int exp_cg_cond(exp_node_t *dest, exp_node_t *left_side, exp_node_t *right_side)
   left_string = exp_format_node(left_side);
   right_string = exp_format_node(right_side);
   dest_string = exp_format_node(dest);
-  // cg_format_label(globals.cur_function->name, "end", globals.nested_indentifier, -1)
-  label = cg_format_label("funkcia_1", "if", globals.nested_count, globals.label_idx); //TODO generovat label
+  label = cg_format_label(globals.cur_function->name, "end", globals.nested_count, -1);
+  //label = cg_format_label("funkcia_1", "if", globals.nested_count, globals.label_idx); //TODO generovat label
   if (label == NULL || dest_string == NULL || right_string == NULL || left_string == NULL) return COMPILER_ERR;
 
   switch(dest->type){
